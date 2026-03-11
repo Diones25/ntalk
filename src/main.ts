3e0d66a12a1ffcf049bad3563235d993fe318069
Diones <pereiradiones987@gmail.com>
2026-03-11 18:51:08 -0300

    configurando o template view ejs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'node:path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // Definimos o tipo da aplicação como NestExpressApplication
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Define onde ficarão os arquivos públicos (CSS, JS, Imagens)
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+
+  // Define a pasta das views e o motor de renderização
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('ejs');
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
