7f81a9b7e46ce6964eb068622c4959e9d20cd115
Diones <pereiradiones987@gmail.com>
2026-03-11 18:55:05 -0300

    modulo de chat

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), ChatModule],
   controllers: [],
   providers: [],
 })
